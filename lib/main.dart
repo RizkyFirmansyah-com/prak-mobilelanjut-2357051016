7a772efb988c7dc53bddbd3e3bc292b1a17aca50
rizky.firmansyahh.2308005 <rizky.firmansyahh.2308005@gmail.com>
2025-09-02 07:42:38 +0000

    firt commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Praktikum Mobile Lanjut'),
+        ),
+        body: const Center(
+          
+          child: Text(
+            'Halo nama saya rizky firmansyah',
+            textAlign: TextAlign.center,
+            
+          ),
+            child: Text(
+            'halo saya ada dua',
+            textAlign: TextAlign.left,
+          )
+        ),
+      )
+    );
+  }
+}
