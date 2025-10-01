3bec597f41d3e5caaa6fa64f6e26302b72a19e3f
rizky.firmansyahh.2308005 <rizky.firmansyahh.2308005@gmail.com>
2025-10-01 05:52:41 +0000

    Membuat Tampilan Dashboard

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:myapp/main.dart';        // sesuaikan 'myapp' dengan name di pubspec.yaml
+import 'package:myapp/main.dart'; // sesuaikan 'myapp' dengan name di pubspec.yaml
 import 'package:myapp/screens/login_screen.dart';
 
 void main() {
